016c48861ae04267945de1cb1da55723bc6efb73
chris <crislm1322@gmail.com>
2021-03-05 12:12:14 -0500

    adding jwt in the server

---
+++ server/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('authenticated', [AuthController::class, 'authenticated']);
+});
