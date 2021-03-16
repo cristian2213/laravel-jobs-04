11696f608c60daba33dd8d858f8d4b677cfaf5fb
chris <crislm1322@gmail.com>
2021-03-16 02:16:36 -0500

    attached all files with the new content added

---
+++ server/database/migrations/2021_03_13_071814_create_vacantes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVacantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->float('salary', 8, 2); // max= 8 digitos y decimales = 2
+            $table->text('benetifs')->nullable();
+            $table->integer('vacancies');
+            $table->text('requirements');
+            $table->text('functionalities');
+
+
+            // relationship
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacantes');
+    }
+}
